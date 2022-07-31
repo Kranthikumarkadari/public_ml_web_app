fa48606ddaa9748e07fbdd46a64ac5eb2eb2c875
Kranthi Kumar Kadari <96544842+TRIPLEK182@users.noreply.github.com>
2022-07-31 14:51:24 +0100

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -20,13 +20,13 @@ from streamlit_option_menu import option_menu
 #loading the saved models
 
 
-diabetes_model = pickle.load(open('C:/Users/kadar/OneDrive/Desktop/MDP SYSTEM/Saved Models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 
-heart_model = pickle.load(open('C:/Users/kadar/OneDrive/Desktop/MDP SYSTEM/Saved Models/heart_disease_model.sav', 'rb'))
+heart_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
 
-parkinsons_model = pickle.load(open('C:/Users/kadar/OneDrive/Desktop/MDP SYSTEM/Saved Models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 #creating the sidebars for navigation
 
